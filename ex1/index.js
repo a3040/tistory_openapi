f30ad917ff38848b4d4ba4c070eac3e311643788
lee <a3040kr@gmail.com>
2023-05-08 17:37:24 +0900

    first commit

---
+++ ex1/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import axios from 'axios';
+
+const app = express();
+
+const client_id = 'a2bd78f8bc5789afa307f11f01a5ab88';
+const redirect_uri='https://a3040.tistory.com/';
+const state_param ='dummy';
+
+app.get('/', (req, res) => {
+    const loc=`https://www.tistory.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&state=${state_param}`;
+
+    console.log( loc );
+
+  res.send(`Hello, World!
+    <a href=${loc}>티스토리로긴</a>`);
+});
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
+
+console.log(`Hello`);
